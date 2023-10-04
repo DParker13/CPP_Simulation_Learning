de8d37643e4bcfa136c2c4c208eeeec2d582cbec
Dparker13 <danielparker134@gmail.com>
2023-10-03 23:24:32 -0400

    Added starter files

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+// helloworld.cpp
+// Traditional hello world program
+// By Daniel Parker
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Hello World!\n";
+}
